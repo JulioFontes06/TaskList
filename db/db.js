dd3f9e6cd44029de2d921baf6b7afcda93f69076
JulioFontes06 <julio.developer06@gmail.com>
2024-07-06 14:57:09 -0300

    Data base turn

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize")
 
-const connection = new Sequelize('tasklist', 'root', 'lbatatam23@', {
+const connection = new Sequelize('tasklist', 'root', '1234@', {
     host: 'localhost',
     dialect: "mysql"
 })
